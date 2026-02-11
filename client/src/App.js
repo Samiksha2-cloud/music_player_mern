96a196f7b6909cfa3fa41f3ba57e8079ff567cf0
Samiksha2-cloud <samikshayadav112@gmail.com>
2026-02-12 03:08:39 +0530

    Integrated BrowserRouter in index.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 function App() {
   return (
     <div className="w-screen h-screen bg-blue-300 flex justify-center items-center">
-       App
+       Music Player Project Started!
     </div>
   );
 }
